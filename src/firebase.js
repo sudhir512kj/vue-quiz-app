37fa24db8e8ad977bdef86d16fbc59c3615d11ee
sudhir meena <sureshmeena512@gmail.com>
2018-04-16 13:11:32 +0530

    credentials removed

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import '@firebase/auth';
 import '@firebase/firestore';
 
 firebase.initializeApp({
-    apiKey: "AIzaSyCsHl0QXyMAMUgLM3ySKSEYfJl3VW8hL_0",
-    authDomain: "vue-quiz-c22e4.firebaseapp.com",
-    databaseURL: "https://vue-quiz-c22e4.firebaseio.com",
-    projectId: "vue-quiz-c22e4",
-    storageBucket: "vue-quiz-c22e4.appspot.com",
-    messagingSenderId: "747915147196"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: ""
 });
 
 export default firebase;
